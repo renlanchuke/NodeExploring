6409d0471e2a52d8d3e2384d61b79d4ce3ef5a7b
renlanchuke <1065730562@qq.com>
2016-03-29 13:36:58 +0800

    FirstNode

---
+++ NodeExploring/FirstNode/discriminant.js
@@ -0,0 +1,4 @@
+//判别式module对象，用于判断方程是否有解
+module.exports = function(a,b,c){
+	return (b*b - 4*a*c);
+};
---
+++ NodeExploring/FirstNode/solve.js
@@ -0,0 +1,27 @@
+//加载求方程根模块
+var quad = require('./quadratic');
+//prompt是由一个命令行提示工具
+var prompt = require('prompt');
+
+prompt.get(['a', 'b', 'c'], function (err, result) {
+    if (err) { return onErr(err); }
+
+    console.log('变量被赋值为:');
+    console.log('a: ' + result.a);
+    console.log('b: ' + result.b);
+    console.log('c:' + result.c);
+
+    //求根，输出结果
+    quad(result.a, result.b, result.c, function (err, quadsolve) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("方程根为 " + quadsolve.root1() + "  " + quadsolve.root2());
+        }
+    });
+
+
+});
+
+
