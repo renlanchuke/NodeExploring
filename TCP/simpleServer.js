72d737651178508a898ef204b2818a6942ffb3fe
renlanchuke <1065730562@qq.com>
2016-05-14 21:21:03 +0800

    simpleServer

---
+++ TCP/simpleServer.js
@@ -0,0 +1,24 @@
+var net = require('net');
+
+var server = net.createServer((socket) => {
+    console.log('new connection');
+
+    socket.setEncoding('utf8');
+
+    socket.write("Hello! You can start typing. Type 'quit' to exit.\n");
+
+    socket.on('data', (data) => {
+        console.log('got:', data.toString());
+        if (data.trim().toLowerCase() === 'quit') {
+            socket.write('Bye bye');
+            return socket.end();
+        }
+        socket.write(data);
+    });
+
+    socket.on('end', function () {
+        console.log('Client connection ended');
+    });
+});
+
+server.listen(4001);
